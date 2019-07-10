0d9b5466a4703182d56911cd32b342aba0660178
Casey <caseyclinga@gmail.com>
2019-07-10 14:09:42 -0700

    Added span for scores to display

--- sunny-clicky-game/src/components/Header.js
+++ sunny-clicky-game/src/components/Header.js
@@ -4,8 +4,7 @@ const Header = props => (
     <nav class="navbar navbar-light bg-light">
         <a class="navbar-text" >Memory Game</a>
         <span class="navbar-text">
-            Current Score: {props.current} <br/> High Score: {props.high}</span>
-        
+            Current Score: {props.current} <br/> High Score: {props.high}</span> 
     </nav>
 );
 
