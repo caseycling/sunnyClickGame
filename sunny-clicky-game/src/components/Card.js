d97cfe5f123ef36361beca448ce9be17be9af688
Casey <caseyclinga@gmail.com>
2019-07-10 14:49:25 -0700

    Added clicked and id props

--- sunny-clicky-game/src/components/Card.js
+++ sunny-clicky-game/src/components/Card.js
@@ -5,7 +5,7 @@ let Card = props => (
     <div className = "card mb-4" >
         <div className="card-body">
             <p className="card-text">Card content</p>
-            <img alt={props.name} src={props.src} />
+            <img alt={props.name} src={props.src} id={props.id} cliked={props.clicked}/>
         </div>
     </div>
 );
