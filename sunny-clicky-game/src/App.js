240d20e39b42d0a36c04f681f1bb8832af56cf54
Casey <caseyclinga@gmail.com>
2019-07-10 12:06:53 -0700

    Adjusted props name to render image as src

--- sunny-clicky-game/src/App.js
+++ sunny-clicky-game/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import DisplayCharacters from './components/index'
+import Images from './characters.json'
 
 function App() {
-  return <DisplayCharacters />;
+  return <DisplayCharacters 
+    src={Images[0].image}
+    
+  />;
 }
+
+console.log(Images[0].image)
 
 export default App;
