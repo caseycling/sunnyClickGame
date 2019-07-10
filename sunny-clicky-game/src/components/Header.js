438d782df288dca5e66a4963ea858f0085b841fa
Casey <caseyclinga@gmail.com>
2019-07-10 12:50:13 -0700

    Add header component

---
+++ sunny-clicky-game/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = props => (
+
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" >Memory Game</a>
+  <span></span>
+</nav>
+);
+
+export default Header
