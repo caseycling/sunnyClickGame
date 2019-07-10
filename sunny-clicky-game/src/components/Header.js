c1e5e5ff7729ecdb48f9edbd280ca5e6afc9c15e
Casey <caseyclinga@gmail.com>
2019-07-10 13:12:00 -0700

    Created input for current and high score

--- sunny-clicky-game/src/components/Header.js
+++ sunny-clicky-game/src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Header = props => (
-
-<nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" >Memory Game</a>
-  <span></span>
-</nav>
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-text" >Memory Game</a>
+        <span class="navbar-text">
+            Current Score: {props.current} <br/> High Score: {props.high}</span>
+        
+    </nav>
 );
 
 export default Header
