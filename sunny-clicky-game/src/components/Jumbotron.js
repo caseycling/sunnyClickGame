24f7211551fee89e73c634fc4d32a4feb5c21caf
Casey <caseyclinga@gmail.com>
2019-07-10 13:18:44 -0700

    Seperated card into seperate component and renamed file

---
+++ sunny-clicky-game/src/components/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function DisplayCharacters()) {
+    return (
+        <div className="container">
+            <div className="jumbotron">
+                <h1>It's Always Sunny In Philadelphia Memory Game</h1>
+                <p>I'm a jumbotron!</p>
+                <p>
+                    <button className="btn btn-primary btn-lg">Learn more</button>
+                </p>
+            </div>
+        </div>
+
+    );
+}
+
+export default DisplayCharacters
